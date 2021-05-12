33b1af38b9ab2a8b9f225ec42de0a78e1add3a88
ccluo33 <ccluo33@gmail.com>
2021-05-12 10:48:48 -0400

    add project 'Pool' and add 21 bugs for project 'Validator'

--- framework/core/Project/Validator.pm
+++ framework/core/Project/Validator.pm
@@ -85,7 +85,86 @@ sub _post_checkout {
             Utils::exec_cmd("cp $build_files_dir/* $work_dir", "Copy generated Ant build file") or die;
         }
     }
+
+     if (-e "$work_dir/build.xml"){
+        rename("$work_dir/build.xml", "$work_dir/build.xml".'.bak');
+        open(IN, '<'."$work_dir/build.xml".'.bak') or die $!;
+        open(OUT, '>'."$work_dir/build.xml") or die $!;
+        while(<IN>) {
+
+            $_ =~ s/compile-tests/compile\.tests/g;
+            #$_ =~ s/classesdir/classes\.dir/g;
+            #$_ =~ s/testclasses\.dir/test\.classes\.dir/g;
+            #$_ =~ s/<pathelement location="\${download\.lib\.dir}\/commons-logging\.jar"\/>/<pathelement location="\${download\.lib\.dir}\/commons-logging\.jar"\/> <pathelement location="\${download\.lib\.dir}\/commons-collections\.jar"\/>/g;
+            
+            $_ =~ s/http:\/\/www\.ibiblio\.org\/maven\/commons-logging\/jars\/commons-logging-1\.0\.3\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-logging\/commons-logging\/1\.0\.3\/commons-logging-1\.0\.3\.jar/g;
+            $_ =~ s/http:\/\/www\.ibiblio\.org\/maven\/commons-beanutils\/jars\/commons-beanutils-1\.7\.0\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-beanutils\/commons-beanutils\/1\.7\.0\/commons-beanutils-1\.7\.0\.jar/g;
+            $_ =~ s/http:\/\/mirrors\.ibiblio\.org\/pub\/mirrors\/maven\/commons-beanutils\/jars\/commons-beanutils-1\.7\.0\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-beanutils\/commons-beanutils\/1\.7\.0\/commons-beanutils-1\.7\.0\.jar/g;
+            $_ =~ s/http:\/\/repo\.maven\.apache\.org\/maven2\/commons-beanutils\/commons-beanutils\/1\.9\.2\/commons-beanutils-1\.9\.2\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-beanutils\/commons-beanutils\/1\.9\.2\/commons-beanutils-1\.9\.2\.jar/g;
+            
+            $_ =~ s/https:\/\/repo\.maven\.org*/https:\/\/repo1\.maven\.org*/g;
+            
+            $_ =~ s/http:\/\/www\.ibiblio\.org\/maven\/commons-logging\/jars\/commons-logging-1\.0\.4\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-logging\/commons-logging\/1\.0\.4\/commons-logging-1\.0\.4\.jar/g;
+            $_ =~ s/http:\/\/mirrors\.ibiblio\.org\/pub\/mirrors\/maven\/commons-logging\/jars\/commons-logging-1\.1\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-logging\/commons-logging\/1\.1\/commons-logging-1\.1\.jar/g;
+            $_ =~ s/http:\/\/repo\.maven\.apache\.org\/maven2\/commons-logging\/commons-logging\/1\.2\/commons-logging-1\.2\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-logging\/commons-logging\/1\.2\/commons-logging-1\.2\.jar/g;
+            
+            $_ =~ s/http:\/\/www\.ibiblio\.org\/maven\/commons-digester\/jars\/commons-digester-1\.8\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-digester\/commons-digester\/1\.8\/commons-digester-1\.8\.jar/g;
+            $_ =~ s/https:\/\/repo\.maven\.org\/maven2\/commons-digester\/commons-digester\/1\.8\/commons-digester-1\.8\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-digester\/commons-digester\/1\.8\/commons-digester-1\.8\.jar/g;
+            $_ =~ s/http:\/\/mirrors.ibiblio.org\/pub\/mirrors\/maven\/commons-digester\/jars\/commons-digester-1\.8\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-digester\/commons-digester\/1\.8\/commons-digester-1\.8\.jar/g;
+            $_ =~ s/http:\/\/mirrors.ibiblio.org\/maven\/commons-digester\/jars\/commons-digester-1\.8\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-digester\/commons-digester\/1\.8\/commons-digester-1\.8\.jar/g;
+            $_ =~ s/http:\/\/repo\.maven\.apache\.org\/maven2\/commons-digester\/commons-digester\/1\.8\.1\/commons-digester-1\.8\.1\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-digester\/commons-digester\/1\.8\.1\/commons-digester-1\.8\.1\.jar/g;
+
+            
+            
+            $_ =~ s/http:\/\/www\.ibiblio\.org\/maven\/commons-digester\/jars\/commons-digester-1\.6\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-digester\/commons-digester\/1\.6\/commons-digester-1\.6\.jar/g;
+            $_ =~ s/http:\/\/www\.ibiblio\.org\/maven\/oro\/jars\/oro-2\.0\.8\.jar/https:\/\/repo1\.maven\.org\/maven2\/oro\/oro\/2\.0\.8\/oro-2\.0\.8\.jar/g;
+            $_ =~ s/http:\/\/dojotoolkit.org\/svn\/dojo\/trunk\/buildscripts\/lib\/custom_rhino.jar/https:\/\/repo1\.maven\.org\/maven2\/org\/dojotoolkit\/custom_rhino\/0\.9\.0\/custom_rhino-0\.9\.0\.jar/g;
+            
+            $_ =~ s/http:\/\/repo\.maven\.apache\.org\/maven2\/commons-collections\/commons-collections\/3\.2\.1\/commons-collections-3\.2\.1\.jar/https:\/\/repo1\.maven\.org\/maven2\/commons-collections\/commons-collections\/3\.2\.1\/commons-collections-3\.2\.1\.jar/g;
+            #support java8
+            $_ =~ s/fork="false"/fork="true"/g;
+            print OUT $_;
+        }
+        close(IN);
+        close(OUT);
+    }
+
+    if (-e "$work_dir/maven-build.xml"){
+        rename("$work_dir/maven-build.xml", "$work_dir/maven-build.xml".'.bak');
+        open(IN, '<'."$work_dir/maven-build.xml".'.bak') or die $!;
+        open(OUT, '>'."$work_dir/maven-build.xml") or die $!;
+        while(<IN>) {
+            $_ =~ s/compile-tests/compile\.tests/g;
+            #$_ =~ s/classesdir/classes\.dir/g;
+            #$_ =~ s/testclasses\.dir/test\.classes\.dir/g;
+            
+            
+            #support java8
+            $_ =~ s/fork="false"/fork="true"/g;
+            print OUT $_;
+        }
+        close(IN);
+        close(OUT);
+    }
+    #exclude the test you don't need
+    if (-e "$work_dir/src/test/org/apache/commons/validator/EmailTest.java"){
+        rename("$work_dir/src/test/org/apache/commons/validator/EmailTest.java", "$work_dir/src/test/org/apache/commons/validator/EmailTest.java".".bak");
+        open(OUT, '>'."$work_dir/src/test/org/apache/commons/validator/EmailTest.java") or die $!;
+        my $converted_file = `iconv -f iso-8859-1 -t utf-8 "$work_dir/src/test/org/apache/commons/validator/EmailTest.java.bak"`;
+        print OUT $converted_file;
+        close(OUT);
+    }
+    if (-e "$work_dir/src/test/java/org/apache/commons/validator/EmailTest.java"){
+        rename("$work_dir/src/test/java/org/apache/commons/validator/EmailTest.java", "$work_dir/src/test/java/org/apache/commons/validator/EmailTest.java".".bak");
+        open(OUT, '>'."$work_dir/src/test/java/org/apache/commons/validator/EmailTest.java") or die $!;
+        my $converted_file = `iconv -f iso-8859-1 -t utf-8 "$work_dir/src/test/java/org/apache/commons/validator/EmailTest.java.bak"`;
+        print OUT $converted_file;
+        close(OUT);
+    }
+     
+
 }
+
 
 #
 # This subroutine is called by the bug-mining framework for each revision during
@@ -98,8 +177,33 @@ sub initialize_revision {
 
     my $work_dir = $self->{prog_root};
     my $result = _ant_layout($work_dir) // _maven_layout($work_dir);
-    die "Unknown layout for revision: ${rev_id}" unless defined $result;
-
+    
+   if (-e "$work_dir/src/main/java" and -e "$work_dir/src/test/java"){
+        $result = {src=>"src/main/java", test=>"src/test/java"} unless defined $result;
+    }
+    elsif (-e "$work_dir/src/main/java" and -e "$work_dir/src/tests/java"){
+        $result = {src=>"src/main/java", test=>"src/tests/java"} unless defined $result;
+    }
+    elsif (-e "$work_dir/src/main" and -e "$work_dir/src/testcases"){
+        $result = {src=>"src/main", test=>"src/testcases"} unless defined $result;
+    }
+    elsif (-e "$work_dir/src/main" and -e "$work_dir/src/tests/junit"){
+        $result = {src=>"src/main", test=>"src/tests/junit"} unless defined $result;
+    }
+    elsif (-e "$work_dir/src/main" and -e "$work_dir/src/tests"){
+        $result = {src=>"src/main", test=>"src/tests"} unless defined $result;
+    }
+    elsif (-e "$work_dir/src/java" and -e "$work_dir/src/test"){
+        $result = {src=>"src/java", test=>"src/test"} unless defined $result;
+    }
+    elsif (-e "$work_dir/src/java" and -e "$work_dir/src/tests"){
+        $result = {src=>"src/java", test=>"src/tests"} unless defined $result;
+    }
+    else {
+        system("tree -d $work_dir");
+        die "Unknown directory layout" unless defined $result;
+    }
+    
     $self->_add_to_layout_map($rev_id, $result->{src}, $result->{test});
     $self->_cache_layout_map(); # Force cache rebuild
 }
