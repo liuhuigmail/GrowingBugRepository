ee389f09fe7c19697f237a80765592513c4385fe
ccluo33 <lll729255684@163.com>
2021-09-07 12:51:52 +0100

    update repos.sh

--- repos.sh
+++ repos.sh
@@ -4,11 +4,11 @@ cd `dirname $0`
 path=$PWD
 work_dir="$path/project_repos"
 cd $work_dir
-cat ../project_information.txt | while read line
+cat ../framework/bug-mining/bug_mining_projects_info.txt | while read line
 do
     # read project information
     read -ra strarr <<<"$line"
-    project_link=${strarr[0]} 
+    project_link=${strarr[2]} 
     project_name=${strarr[1]} 
     echo "PROJECT_LINK: $project_link"
     echo "PROJECT_NAME: $project_name"
